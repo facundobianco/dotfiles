09551eb885676622a15d1d6b2348c7ba8349d766
Facundo Bianco <vando@van.do>
2016-01-15 12:41:49 -0300

    Conky: added config file for Debian and updated `cwmlistwm.sh`.

--- X11/conky/cwmlistwm.sh
+++ X11/conky/cwmlistwm.sh
@@ -1,27 +1,27 @@
 #!/bin/sh
 
-for ID in `xprop -root | sed '/_LIST(WINDOW)/!d;s/.*# //;s/,//g'`
+for ID in `xprop -root -notype _NET_CLIENT_LIST | sed 's/.*# //'`
 do
-    case `xprop -id "$ID" | sed -n '/WM_CLASS/s/.*= "\([^"]*\)".*/\1/p'` in
+    case `xprop -id "$ID" -notype WM_CLASS | sed 's/.*= "\([^"]*\)".*/\1/'` in
         main) 
             GROUP1="\${color0}1\${color}" 
 	    ;;
-        Navigator|Browser|Places|Dialog|Google*|netsurf) 
+        Navigator|Browser|Places|netsurf|Dialog|Google*) 
             GROUP2="\${color0}2\${color}" 
 	    ;;
         casa)
             GROUP3="\${color0}3\${color}" 
 	    ;;
-        urxvt|xterm|skype)
+        urxvt|xterm|roxterm|skype|yakyak)
             GROUP4="\${color0}4\${color}" 
 	    ;;
-        VCLSalFrame|soffie|libreoffice|zathura|leafpad|feh|SciTE)
+        VCLSalFrame|soffie|zathura|leafpad|feh|libreoffice|scite|SciTE|mirage|cr3|diffuse|mcomix|mtpaint)
             GROUP5="\${color0}5\${color}" 
 	    ;;
-        gtkpod|kicad|linphone|gimp|mplayer|dia|globaltime|spotify)
+        gtkpod|kicad|linphone|gimp|mplayer|dia|dia-normal|globaltime|spotify|xfburn|arandr|mpv|MPlayer)
             GROUP6="\${color0}6\${color}" 
 	    ;;
-	rdesktop|Qt-subapplication|sun-awt-X11-XFramePeer)
+	rdesktop|Qt-subapplication|sun-awt-X11-*)
 	    GROUP7="\${color0}7\${color}" 
 	    ;;
 	"") # This prevents when all windows are closed
