405a85a49c4de5e72f93502068618a38bae14ef3
Facundo A Bianco <Facundo.Bianco@gds.ey.com>
2019-03-11 14:30:09 -0300

    Added GIT Bash custom prompt

---
+++ mswindows/misc/git-prompt.sh
@@ -0,0 +1,22 @@
+# This file should be placed on ~/.config/git
+
+cd ~/repos
+
+PS1='\[\033[33m\]'       # change to brownish yellow
+PS1="$PS1"'\W'           # current working directory
+if test -z "$WINELOADERNOEXEC"
+then
+	GIT_EXEC_PATH="$(git --exec-path 2>/dev/null)"
+	COMPLETION_PATH="${GIT_EXEC_PATH%/libexec/git-core}"
+	COMPLETION_PATH="${COMPLETION_PATH%/lib/git-core}"
+	COMPLETION_PATH="$COMPLETION_PATH/share/git/completion"
+	if test -f "$COMPLETION_PATH/git-prompt.sh"
+	then
+		. "$COMPLETION_PATH/git-completion.bash"
+		. "$COMPLETION_PATH/git-prompt.sh"
+		PS1="$PS1"'\[\033[36m\]'  # change color to cyan
+		PS1="$PS1"'`__git_ps1`'   # bash function
+	fi
+fi
+PS1="$PS1"'\[\033[0m\]'  # change color
+PS1="$PS1"' $> '         # prompt: always $
