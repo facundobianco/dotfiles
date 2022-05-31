85d3e274465289ef639f0703ad96fc9a4259c1bf
Facundo Bianco <facundobianco1984@gmail.com>
2022-05-31 11:28:46 -0300

    Updated Kitty files

--- ncurses/kitty/googlesearch.sh
+++ ncurses/kitty/googlesearch.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-BROWSER="/opt/google/chrome/google-chrome"
-URL="http://www.google.co.uk/search?q="
+BROWSER="xdg-open"
+URL="https://www.google.com/search?q="
 PRE_FORMAT_QUERY=$1
 QUERY=$(echo $PRE_FORMAT_QUERY | sed -e "s/\ /+/g")
 
