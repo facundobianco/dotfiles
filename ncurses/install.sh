926d6cb98ff97a994a18f3b9928e16246fba583c
Facundo Bianco <facundobianco1984@gmail.com>
2023-03-20 16:11:36 +0000

    Updated install for Linux

--- ncurses/install.sh
+++ ncurses/install.sh
@@ -1,24 +1,32 @@
 #!/bin/bash
 
+# variables
 VDIR="${HOME}/Code/vando/dotfiles/ncurses"
-MDIR="${HOME}/Code/misc"
 
+# required dirs
 mkdir -p ~/.bash ~/.git ~/.kube
 
 # bash
 ln -sf ${VDIR}/bash/profile ~/.profile
 find ${VDIR}/bash -type f \( ! -iname profile \) -exec ln -sf {} ~/.bash/ \;
-ln -sf ${MDIR}/kubectl-aliases/.kubectl_aliases ~/.kube/aliases
+wget -O ~/.kube/aliases \
+  https://github.com/ahmetb/kubectl-aliases/blob/master/.kubectl_aliases
 
 # git
 ln -sf ${VDIR}/git/gitconfig ~/.gitconfig
 ln -sf ${VDIR}/git/excludes ~/.git/excludes
-ln -sf ${MDIR}/git/contrib/completion/git-completion.bash ~/.git/completion
 ln -sf ${VDIR}/git/envs ~/.git/envs
-if [ ! -s ~/Code/.gitconfig ]; then
+wget -O ~/.git/completion \
+  https://github.com/git/git/blob/master/contrib/completion/git-completion.bash
+if [ ! -s ~/Code/.gitconfig ]
+then
   echo "[user]" > ~/Code/.gitconfig
   echo "${N:-} ${N:-} ${N:-}e${N:-}m${N:-}a${N:-}i${N:-}l${N:-} ${N:-}=${N:-} ${N:-}f${N:-}a${N:-}c${N:-}u${N:-}n${N:-}d${N:-}o${N:-}b${N:-}i${N:-}a${N:-}n${N:-}c${N:-}o${N:-}1${N:-}9${N:-}8${N:-}4${N:-}@${N:-}g${N:-}m${N:-}a${N:-}i${N:-}l${N:-}.${N:-}c${N:-}o${N:-}m" >> ~/Code/.gitconfig
 fi
 
-# avoid last login msg
-touch ~/.hushlogin
+# macOS
+if [ `uname -s` == "Darwin" ]
+then
+  # avoid last login msg
+  touch ~/.hushlogin
+fi
